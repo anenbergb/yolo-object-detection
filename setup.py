f8deabaa20e5a9069eb2ce165469032cb77f8472
Bryan Anenberg <anenbergb@gmail.com>
2025-02-04 12:14:05 -0800

    established setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yolo",
+    version="1.0.0",
+    url="https://github.com/anenbergb/MBAS",
+    author="Bryan Anenberg",
+    author_email="anenbergb@gmail.com",
+    description="A fully from scratch implementation of a Yolo style object detector",
+    python_requires=">=3.10",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tensorboard",
+        "pandas",
+        "pandas-stubs",
+        "opencv-python",
+        "loguru",
+        "matplotlib",
+        "ffmpeg-python",
+        "tqdm",
+        "types-tqdm",
+        "pillow",
+        "types-Pillow",
+        "tabulate",
+    ],
+    extras_require={
+        "torch": [
+            "torch",
+            "torchvision",
+        ],
+        "notebook": [
+            "jupyter",
+            "itkwidgets",
+            "jupyter_contrib_nbextensions",
+            "plotly",
+            "seaborn",
+            "fiftyone",
+        ],
+        "dev": ["black", "mypy", "flake8", "isort", "ipdb"],
+    },
+)
