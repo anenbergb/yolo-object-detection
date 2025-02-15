9b3e81c936d02216077671c78393bfa2778593cc
Bryan Anenberg <anenbergb@gmail.com>
2025-02-15 12:18:35 -0800

    Implemented CollateWithAnchors to construct the label tensors

---
+++ yolo/anchors.py
@@ -0,0 +1,291 @@
+import torch
+from typing import List, Tuple
+import itertools
+from collections import Counter
+from torchvision.ops import box_iou
+
+
+################################
+# Create a Lx4 anchor box grid
+# where for an inupt image of size (416,608)
+# and feature pyramids downsampling by factors x8, x16, x32 to (52,76), (26,38), (13,19)
+# then L = 52*76*3 + 26*38*3 + 13*19*3
+###############################
+
+
+def make_spatial_anchors_per_scale(
+    anchors_list: List[Tuple[int, int]],
+    scale: int,
+    image_height: int,
+    image_width: int,
+    verbose=False,
+):
+    """
+    Create a Lx4 anchor box grid where for an input image of size (416,608)
+    and feature pyramids downsampling by factors x8, x16, x32 to (52,76), (26,38), (13,19)
+    then L = 52*76*3 + 26*38*3 + 13*19*3
+    Need the anchor boxes to be in the original image resolution
+    """
+
+    anchor_halfs = torch.tensor(anchors_list, dtype=torch.float) / 2  # w/2, h/2
+
+    x_axis = torch.arange(0, image_width, scale)
+    y_axis = torch.arange(0, image_height, scale)
+    grid_y, grid_x = torch.meshgrid(y_axis, x_axis, indexing="ij")  # .shape [13,19]
+    grid_xy = torch.stack([grid_x, grid_y], dim=-1)  # .shape [13, 19, 2]
+    grid_xy_centers = grid_xy + 0.5 * scale  # center the anchor boxes in each grid cell
+
+    # Expand grid_centers and anchors to enable broadcasting
+    grid_centers_expanded = grid_xy_centers.unsqueeze(-2)  # Shape: (13, 19, 1, 2)
+    anchor_halfs_expanded = anchor_halfs.unsqueeze(0).unsqueeze(
+        0
+    )  # Shape: (1, 1, 3, 2)
+    # Compute the upper-left xy and lower-right xy coordinates in a vectorized manner
+    # to get 2x tensors of shape (13,19,3,2), then concatenated to (13,19,3,4)
+    grid_anchors_xyxy = torch.cat(
+        [
+            grid_centers_expanded - anchor_halfs_expanded,
+            grid_centers_expanded + anchor_halfs_expanded,
+        ],
+        axis=-1,
+    )
+    grid_anchors_xyxy_flatten = grid_anchors_xyxy.flatten(
+        end_dim=-2
+    )  # .shape (13*19*3, 4)
+
+    if verbose:
+        print(
+            f"[make_spatial_anchors][scale: /{scale}] image resized from ({image_height}, {image_width}) to ({image_height / scale}, {image_width / scale})"
+        )
+        s1, s2, s3 = grid_anchors_xyxy.shape[:3]
+        print(
+            f"[make_spatial_anchors][scale: /{scale}] L={s1}x{s2}x{s3}={s1*s2*s3} spatial_anchors is (L,4)={tuple(grid_anchors_xyxy_flatten.shape)}"
+        )
+    return grid_anchors_xyxy_flatten
+
+
+def make_spatial_anchors(
+    anchors_list: List[Tuple[int, int]],
+    scales: List[int],
+    image_height: int,
+    image_width: int,
+    num_anchors_per_scale: int = 3,
+    verbose: bool = False,
+):
+    anchors_per_scale = list(itertools.batched(anchors_list, num_anchors_per_scale))
+    spatial_anchors_per_scale = [
+        make_spatial_anchors_per_scale(
+            anchors, scale, image_height, image_width, verbose=verbose
+        )
+        for anchors, scale in zip(anchors_per_scale, scales)
+    ]
+    spatial_anchors = torch.cat(spatial_anchors_per_scale, axis=0)
+    return spatial_anchors
+
+
+def make_scale_map(
+    scales: List[int],
+    image_height: int,
+    image_width: int,
+    num_anchors_per_scale: int = 3,
+):
+    # scale at each position (L,)
+    return torch.cat(
+        [
+            torch.full(
+                (int(image_height * image_width * num_anchors_per_scale / scale**2),),
+                scale,
+            )
+            for scale in scales
+        ]
+    )
+
+
+def make_anchor_map(
+    anchors_list: List[Tuple[int, int]],
+    scales: List[int],
+    image_height: int,
+    image_width: int,
+    num_anchors_per_scale: int = 3,
+):
+    anchors_per_scale = list(itertools.batched(anchors_list, num_anchors_per_scale))
+    anchors_maps = []
+    for anchors, scale in zip(anchors_per_scale, scales):
+        anchors = torch.tensor(anchors, dtype=torch.float)
+        scaled_height = int(image_height / scale)
+        scaled_width = int(image_width / scale)
+        # Reshape anchors to (1, 1, 3, 2) to enable broadcasting
+        anchors_expanded = anchors.unsqueeze(0).unsqueeze(0)
+        # Expand anchors to the desired shape (13, 19, 3, 2)
+        anchors_copied = anchors_expanded.expand(
+            scaled_height, scaled_width, len(anchors), 2
+        )
+        anchors_flat = anchors_copied.flatten(end_dim=-2)
+        anchors_maps.append(anchors_flat)
+    anchors_map = torch.cat(anchors_maps, axis=0)
+    return anchors_map
+
+
+def get_valid_gt_cxcy(
+    boxes: torch.Tensor,
+    scales: List[int],
+    image_height: int,
+    image_width: int,
+    num_anchors_per_scale: int = 3,
+    verbose=False,
+):
+    """
+    Input: boxes are x1y1x2y2
+
+    This function proposes the possible anchor boxes that should be considered
+    for the "best matching" anchor box with the ground truth boxes.
+    In some cases, many anchor boxes at different cell locations (cx,cy) offset
+    from the top left corner of the image may equally overlap with the ground truth
+    box. This could be due to the lack of an anchor box close to the shape of the
+    ground truth box. The "best matching" anchor box that should be selected
+    is the one where the anchor box center (ax,ay) is at most 1 unit greater than
+    the the (cx,cy). This constraints is due to the bounding box prediction
+    encoding proposed in YoloV2, where a predicted boxes center point is given by
+    bx = sigmoid(tx) + cx
+    by = sigmoid(ty) + cy
+    0 < sigmoid(x) <1
+    Because the output of sigmoid is between 0 and 1, the predicted box center (bx,by)
+    is limited to (cx,cy) < (bx,by) < (cx+1,cy+1)
+
+    These calculations are made in the downscaled feature map space
+        e.g. (image_height/scale, image_width/scale)
+
+    The output of this function should be a
+    binary mask (B,L) = 1 (if the spatialized anchor index is a valid "best match")
+                        0  otherwise
+        B is # of boxes in the image
+        L is # of spatialized anchors.
+    """
+    box_centers = boxes[:, :2] + (boxes[:, 2:4] - boxes[:, :2]) / 2
+    binary_masks = []
+    for scale in scales:
+        scaled_height = int(image_height / scale)
+        scaled_width = int(image_width / scale)
+        binary_mask = torch.zeros(
+            len(boxes), scaled_height, scaled_width, num_anchors_per_scale
+        )
+        cxcy = torch.floor(box_centers / scale).to(torch.long)
+        binary_mask[torch.arange(len(boxes)), cxcy[:, 1], cxcy[:, 0]] = 1
+        binary_masks.append(binary_mask.flatten(start_dim=1))
+
+    valid_gt_cxcy = torch.cat(binary_masks, axis=1)
+    if verbose:
+        print(
+            f"valid_gt_cxcy {tuple(valid_gt_cxcy.shape)} {Counter(valid_gt_cxcy.flatten().tolist())}"
+        )
+    return valid_gt_cxcy
+
+
+def make_spatial_anchor_mask(
+    boxes_per_image,
+    spatial_anchors,
+    scales: List[int],
+    image_height: int,
+    image_width: int,
+    num_anchors_per_scale: int = 3,
+    verbose=False,
+):
+    batch_size = len(boxes_per_image)
+
+    if verbose:
+        print(
+            f"[make_spatial_anchor_mask] spatial_anchors.shape {tuple(spatial_anchors.shape)}"
+        )
+        num_boxes_per_image = [len(x) for x in boxes_per_image]
+        num_boxes = sum(num_boxes_per_image)
+        print(
+            f"[make_spatial_anchor_mask][batch_size {batch_size}][# boxes {num_boxes}] boxes per image: {num_boxes_per_image}"
+        )
+
+    # >= 0 index into gt boxes (highest IOU) per image
+    # -1: ignore (IOU > 0.5)
+    # -2: negative examples. anchors with IOU <= 0.5
+    spatial_anchor_mask = -2 * torch.ones(
+        batch_size, len(spatial_anchors), dtype=torch.long
+    )
+
+    for image_idx, boxes in enumerate(boxes_per_image):
+
+        # both sets of boxes are expected to be in (x1, y1, x2, y2) format with 0 <= x1 < x2 and 0 <= y1 < y2.
+        ious = box_iou(boxes, spatial_anchors)
+        # ignore anchors that match, but aren't the maximum
+        _, anchor_idx = torch.where(ious > 0.5)
+        spatial_anchor_mask[image_idx, anchor_idx] = -1
+
+        # (B,L)
+        valid_gt_cxcy = get_valid_gt_cxcy(
+            boxes, scales, image_height, image_width, num_anchors_per_scale
+        )
+        valid_ious = valid_gt_cxcy * ious
+
+        # Find the max value per row
+        max_ious, anchor_idx = torch.max(valid_ious, dim=1, keepdim=True)
+        spatial_anchor_mask[image_idx, anchor_idx] = torch.arange(len(boxes)).unsqueeze(
+            1
+        )
+
+    if verbose:
+        num_best_match = torch.sum(spatial_anchor_mask >= 0).item()
+        num_gt_05 = torch.sum(spatial_anchor_mask == -1).item()
+        num_lt_05 = torch.sum(spatial_anchor_mask == -2).item()
+        print(
+            f"[make_spatial_anchor_mask] spatial_anchor_mask.shape {tuple(spatial_anchor_mask.shape)} [{num_best_match} #best_match][{num_gt_05} # >0.5][{num_lt_05} # <=0.5]"
+        )
+        print(
+            "\tpossible values: [>=0, -1, -2]\n\t>= 0 index into gt boxes (highest IOU) per image\n\t-1: ignore (IOU > 0.5)\n\t-2: negative examples. anchors with IOU <= 0.5"
+        )
+    return spatial_anchor_mask
+
+
+def encode_boxes(boxes_xyxy, scale, anchor, epsilon=1e-5):
+    """
+    box is expected to be in (x1, y1, x2, y2) format with 0 <= x1 < x2 and 0 <= y1 < y2.
+    epsilon is to handle the case of log(0) = -inf
+
+    boxes_xyxy: (B,4)
+    scale: (B,)
+    anchor: (B,2)
+
+
+    The Yolo box encoding equations
+        bx = scale * (sigmoid(tx) + cx)
+        bw = pw * exp(tw), where pw is anchor width
+        (similarly for by and bh)
+    Invert these equations to encode a bxbybwbh box to txtytwth
+        (bx/scale - cx) = sigmoid(tx)
+        let qx = bx/scale - cx
+        tx = log(qx / (1 - qx))
+
+        tw = log(bw / pw)
+
+    Notice that because 0 < sigmoid(tx) < 1,
+        if bx/scale = 7.4, then cx = 7.0, and bx/scale - cx = 0.4
+        bx/scale - cx always equals the fractional remainder
+    """
+    ndim = boxes_xyxy.ndim
+    assert ndim in (1, 2)
+    assert ndim == anchor.ndim
+    if ndim == 1:
+        boxes_xyxy = boxes_xyxy.unsqueeze(0)
+        anchor = anchor.unsqueeze(0)
+
+    boxes_wh = boxes_xyxy[:, 2:4] - boxes_xyxy[:, :2]
+    boxes_center_xy = boxes_xyxy[:, :2] + boxes_wh / 2
+
+    bxy = boxes_center_xy / scale
+    qxy = torch.frac(bxy) + epsilon
+    # using the Yolo box encoding definition, qxy = sigmoid(txty)
+    txy = torch.log(qxy / (1 - qxy))
+
+    # epsilon isn't required since width of the box has to be greater than 0
+    twh = torch.log(boxes_wh / anchor)
+    txywh = torch.cat([txy, twh], axis=1)
+    if ndim == 1:
+        assert txywh.shape[0] == 1
+        txywh = txywh[0]
+    return txywh
--- yolo/data.py
+++ yolo/data.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable, Any, Dict
+from typing import Optional, Callable, Any, Dict, List, Tuple
 import os
 import torch
 from torchvision.transforms import v2
@@ -7,8 +7,26 @@ from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
 from torchvision import tv_tensors
 from torchvision.transforms.v2._utils import _get_fill
 
+from yolo.anchors import (
+    make_spatial_anchors,
+    make_scale_map,
+    make_anchor_map,
+    make_spatial_anchor_mask,
+    encode_boxes,
+)
 
 
+def labels_getter(inputs: Any) -> List[torch.Tensor]:
+    # targets dictionary should be the second element in the tuple
+    if isinstance(inputs, (tuple, list)):
+        inputs = inputs[1]
+
+    label_names = ["class_idx", "class_id", "iscrowd"]
+    return [inputs[label_name] for label_name in label_names]
+
+
+# Other possible transforms to consider
+# https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#sphx-glr-auto-examples-transforms-plot-transforms-e2e-py
 def get_val_transforms(
     resize_size=608,
     mean=(0.485, 0.456, 0.406),
@@ -20,23 +38,39 @@ def get_val_transforms(
             v2.ToImage(),  # convert PIL image to tensor
             v2.ToDtype(torch.uint8, scale=True),
             v2.Resize((resize_size, resize_size)),  # resize the image. bilinear
+            v2.ClampBoundingBoxes(),  # clamp bounding boxes to be within the image
+            v2.SanitizeBoundingBoxes(
+                labels_getter=labels_getter
+            ),  # Remove degenerate/invalid bounding boxes and their corresponding labels and masks.
             v2.ToDtype(torch.float32, scale=True),  # Normalize expects float input
             v2.Normalize(mean=mean, std=std),
             # v2.ToPureTensor(),
         ]
     )
 
+
 class CocoDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_root: str, split: str = "train", transform: Optional[Callable] = None):
+    def __init__(
+        self,
+        dataset_root: str,
+        split: str = "train",
+        transform: Optional[Callable] = None,
+    ):
         assert split in ("train", "validation")
         root = os.path.join(dataset_root, split, "data")
         annFile = os.path.join(dataset_root, split, "labels.json")
-        self.dataset = wrap_dataset_for_transforms_v2(CocoDetection(root = root, annFile = annFile))
+        # https://pytorch.org/vision/main/generated/torchvision.datasets.wrap_dataset_for_transforms_v2.html#torchvision.datasets.wrap_dataset_for_transforms_v2
+        self.dataset = wrap_dataset_for_transforms_v2(
+            CocoDetection(root=root, annFile=annFile),
+            target_keys=["image_id", "boxes", "labels", "iscrowd"],
+        )
         self.transform = transform
 
-        self.class_id2name = {id: d["name"] for id,d in self.dataset.coco.cats.items()}
-        self.class_id2idx = {id: idx for idx, id in enumerate(self.dataset.coco.cats.keys())}
-        
+        self.class_id2name = {id: d["name"] for id, d in self.dataset.coco.cats.items()}
+        self.class_id2idx = {
+            id: idx for idx, id in enumerate(self.dataset.coco.cats.keys())
+        }
+
         self.class_names = [d["name"] for d in self.dataset.coco.cats.values()]
         self.class_idx2id = {idx: id for id, idx in self.class_id2idx.items()}
 
@@ -47,34 +81,133 @@ class CocoDataset(torch.utils.data.Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
+        """
+        target contains:
+            "image_id": int
+            "boxes": torch.tensor (N,4)
+            "labels": torch.tensor (N,)
+
+        updates the target to include:
+            "image_id": int
+            "boxes": torch.tensor (N,4)
+            "class_name": List[str]
+            "class_idx": torch.tensor (N,)
+            "class_id": torch.tensor (N,)
+        """
         img, target = self.dataset[idx]
-        class_ids = target["labels"]
-        target["class_names"] = [self.class_id2name[id.item()] for id in class_ids]
-        target["labels"] = torch.tensor([self.class_id2idx[id.item()] for id in class_ids])
-        target["class_ids"] = class_ids
+        class_ids = target.pop("labels")
+        # target["class_name"] = [self.class_id2name[id.item()] for id in class_ids]
+        target["class_idx"] = torch.tensor(
+            [self.class_id2idx[id.item()] for id in class_ids]
+        )
+        target["class_id"] = class_ids
 
         if self.transform:
             img, target = self.transform(img, target)
         return img, target
-    
 
-class Collate:
-    def __call__(self, batch):
-        """
-        batch should be a list of dictionaries with keys "pil_image", "label", "image", and "class_name"
-        """
-        images = [sample["pil_image"] for sample in batch]
-        for sample in batch:
-            sample.pop("pil_image")
 
-        batch = torch.utils.data.default_collate(batch)
-        batch["pil_image"] = images
+class CollateWithAnchors:
+    """
+    Assume that all the images have been resized to the same size.
+
+    """
+
+    def __init__(
+        self,
+        anchors: List[Tuple[int, int]],
+        scales: List[int],
+        image_height: int,
+        image_width: int,
+        num_anchors_per_scale: int = 3,
+        num_classes: int = 80,
+        verbose: bool = False,
+    ):
+        self.anchors = anchors
+        self.scales = scales
+        self.image_height = image_height
+        self.image_width = image_width
+        self.num_anchors_per_scale = num_anchors_per_scale
+        self.num_classes = num_classes
+        self.verbose = verbose
+        self.spatial_anchors = make_spatial_anchors(
+            anchors, scales, image_height, image_width, num_anchors_per_scale, verbose
+        )
+        self.scale_map = make_scale_map(
+            scales, image_height, image_width, num_anchors_per_scale
+        )
+        self.anchor_map = make_anchor_map(
+            anchors, scales, image_height, image_width, num_anchors_per_scale
+        )
+
+    def make_label_tensors(self, batch_boxes, batch_class_idx):
+        """
+        Inputs:
+            batch_boxes: List[Tensor(B,4)]
+            batch_class_idx: List[Tensor(B,)]
+            B is the number of boxes in the image. List is length N, where N is the number of images in the batch.
+
+        Need to construct label tensors for the objectness, coordinates, and classification losses
+        objectness: (N,L,1) where L = 22743. {1: if spatial_anchor_mask >= 0, 0: if mask == -2}, BCELoss reduction="none", then apply mask to ignore mask == -1
+        coordinates: (N,L,4) where ground-truth encoded txtytwth is only defined for the spatial_anchor_mask >= 0 values. MSELoss reduction="none", then apply mask to ignore mask < 0
+        classification: (N,L,C) where C=80 is one-hot encoding for class at the anchor box, only defined for spatial_anchor_mask >= 0 values. BCELoss reduction="none", then apply mask to ignore mask < 0
+        """
+        spatial_anchor_mask = make_spatial_anchor_mask(
+            batch_boxes,
+            self.spatial_anchors,
+            self.scales,
+            self.image_height,
+            self.image_width,
+            self.num_anchors_per_scale,
+            verbose=self.verbose,
+        )
+
+        objectness_label = (spatial_anchor_mask >= 0).to(torch.long).unsqueeze(-1)
+        coord_label = torch.zeros(*spatial_anchor_mask.shape, 4)
+        classification_label = torch.zeros(*spatial_anchor_mask.shape, self.num_classes)
+
+        for batch_idx, L_idx in zip(*torch.where(spatial_anchor_mask >= 0)):
+            box_idx = spatial_anchor_mask[batch_idx, L_idx]
+
+            # set encoded box
+            scale = self.scale_map[L_idx]
+            anchor = self.anchor_map[L_idx]
+            gt_box = batch_boxes[batch_idx][box_idx]
+            coord_label[batch_idx, L_idx] = encode_boxes(gt_box, scale, anchor)
+
+            # set classification label
+            class_idx = batch_class_idx[batch_idx][box_idx]
+            classification_label[batch_idx, L_idx][class_idx] = 1
+        return {
+            "objectness_label": objectness_label,
+            "coordinates_label": coord_label,
+            "classification_label": classification_label,
+        }
+
+    def __call__(self, batched_image_target):
+
+        batch_list = []
+        for image, target in batched_image_target:
+            batch_list.append(
+                {
+                    "image": image,
+                    "image_id": target["image_id"],
+                }
+            )
+        batch = torch.utils.data.default_collate(batch_list)
+        label_names = ["boxes", "labels", "class_idx", "class_id", "iscrowd"]
+        for label_name in label_names:
+            batch[label_name] = [x[label_name] for x in batched_image_target]
+
+        label_tensors = self.make_label_tensors(batch["boxes"], batch["class_idx"])
+        batch.update(label_tensors)
         return batch
-    
+
+
 class PadToMultipleOf32(v2.Pad):
-    def __init__(self, fill=0, padding_mode='constant'):
+    def __init__(self, fill=0, padding_mode="constant"):
         super().__init__(padding=0, fill=fill, padding_mode=padding_mode)
-    
+
     def transform(self, inpt: Any, params: Dict[str, Any]) -> Any:
         # bounding boxes don't need padding since padding is applied
         # to the bottom and right sides
@@ -85,4 +218,10 @@ class PadToMultipleOf32(v2.Pad):
             pad_height = (32 - height % 32) % 32
             padding = (0, 0, pad_width, pad_height)
         fill = _get_fill(self._fill, type(inpt))
-        return self._call_kernel(v2.functional.pad, inpt, padding=padding, fill=fill, padding_mode=self.padding_mode)
+        return self._call_kernel(
+            v2.functional.pad,
+            inpt,
+            padding=padding,
+            fill=fill,
+            padding_mode=self.padding_mode,
+        )
